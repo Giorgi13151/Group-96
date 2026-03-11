4ed5c311e3bbf497a08ef1e9322150a7774641f0
Giorgi13151 <gio59792@gmail.com>
2026-03-12 00:10:39 +0400

    second commit

---
+++ level 10/homework/pyt.py
@@ -0,0 +1,31 @@
+for i in range(50,201):
+    print(i)
+for i in range(1,100,2):
+    print(i)
+for i in range(100,150,3):
+    print(i)
+num=0
+while num < 50:
+    print(num)
+    num += 1
+    num=20
+    while num <60:
+        print(num)
+        num += 5
+Name = 'Giorgi'
+for i in Name:
+    print(i)
+
+Stock = 300
+
+while Stock >0:
+    Stock -=2
+    if Stock >= 0:
+        print("You have secsessfully bought the drink Remaining:", Stock)
+    if Stock <= 0:
+        print("Out of stock")
+
+for i in range (50,20,-3):
+    print(i)
+
+#while loop არის ციკლი რომელიც შესაძლებლობას გვაძლევს ერთი და იმავე კოდის გაშვებას რამდენჯერმე მისი Counter არის i რომელიც შესაძლებლობას გვაძლევს გავიგოთ რამდენჯერ გამეორდა ციკლი

6eda8585dd41a39d4ef6d7a607bda4b9350cfe85
Giorgi13151 <gio59792@gmail.com>
2026-03-26 19:44:56 +0400

    second commit

---
+++ level 14/homework/index.py
@@ -0,0 +1,19 @@
+phrase = "PythonProgramming"
+print(phrase[0:6])
+print(phrase[6:-1])
+print(phrase[::3])
+
+text = "PythonSlicing"
+print(text[0:6])
+print(text[::-1])
+print(text[::2])
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+print(letters[::2])
+print(letters[2:7:2])
+print(letters[4::-1])
+
+sentence = "Slicing makes Python powerful"
+print(sentence[13:20])
+print(sentence[-23::-1])
+print(sentence[::3])
